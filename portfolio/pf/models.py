9f3b217757d48d49b7e06eeeaa5a726fc8ebfee1
Emmanuel Guefif <eguefif@fastmail.com>
2023-12-02 09:59:54 -0500

    Add project detail page

--- portfolio/pf/models.py
+++ portfolio/pf/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    date = models.DateTimeField("date finished")
-    url = models.URLField(max_length=240)
+    title = models.CharField(max_length=50)
+    date = models.DateField("date finished")
+    github = models.URLField(max_length=240)
+    small_description = models.CharField(max_length=200,
+            default="")
     description = models.TextField(max_length = 2500)
 
     def __str__(self):
         return self.title
+
+class Skill(models.Model):
+    project = models.ManyToManyField(Project)
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=200)
+
+class Technology(models.Model):
+    project = models.ManyToManyField(Project)
+    name = models.CharField(max_length=60)
--- portfolio/pf/views.py
+++ portfolio/pf/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from django.template import loader
+from django.template import loader, Context
 
-from .models import Project
+from .models import Project, Skill, Technology
 
 def index(request):
     projects = Project.objects.order_by("-date")
     context = {
             "projects": projects,
             }
+
     return render(request, "pf/index.html", context)
+
+def project(request, pk):
+    project = Project.objects.get(pk=pk)
+    skills = Skill.objects.all().filter(
+            project__title__contains=project.title)
+    technologies = Technology.objects.all().filter(
+            project__title__contains=project.title)
+    context = {
+            "project": project,
+            "skills": skills,
+            "technologies": technologies,
+            }
+    return render(request, "pf/project.html", context)
