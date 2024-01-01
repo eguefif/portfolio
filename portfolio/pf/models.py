9ec286bd6ae43692686dc30d1f5e6c9dabfbeaec
Emmanuel Guefif <eguefif@fastmail.com>
2024-01-01 20:04:37 +0000

    Added github and linkedin

--- portfolio/pf/models.py
+++ portfolio/pf/models.py
@@ -31,5 +31,8 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.TextField(max_length = 2500)
     image = models.FileField(upload_to="media/", blank=True)
+    resume = models.FileField(upload_to="media/", blank=True)
     role = models.CharField(max_length=50)
     fullname = models.CharField(max_length=50)
+    github = models.URLField(max_length=240, default="")
+    linkedin = models.URLField(max_length=240, default="")
