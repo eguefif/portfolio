46341aed31c61fdc977f2b9236a3d4e414914ccc
Emmanuel Guefif <eguefif@gmail.com>
2024-01-21 19:58:50 -0500

    Set settings.py for dev env

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -124,5 +124,5 @@ STATIC_ROOT = BASE_DIR / "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-MEDIA_ROOT = BASE_DIR / "uploads/"
+MEDIA_ROOT = BASE_DIR / "uploads/media/"
 MEDIA_URL = "media/"
